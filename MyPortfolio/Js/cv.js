72b53d9cc4b638024b5818bbf2d408a9ef876a2a
Vignesh P <97089673+VikkiVignesh@users.noreply.github.com>
2024-01-28 13:46:52 +0530

    Add files via upload

---
+++ MyPortfolio/Js/cv.js
@@ -0,0 +1,8 @@
+/**Typing js animation */
+
+var typed=new Typed(".typing",{
+    strings:["Web Designer","Web Developer","Graphic Designer","Problem Solver"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
